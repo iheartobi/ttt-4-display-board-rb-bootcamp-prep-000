dfc426e13bbb826e184261a1e56131126c7a0ecc
Ititi Obidah <iobidah9@gmail.com>
2019-04-25 20:45:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,7 +9,7 @@ def display_board(board)
   puts "   |   |   "
 end 
 
-board = [" ", " ", " ", " ","X", " ", " ", " ", " "]
+board = [" ", " ", " ", " ","#{X}", " ", " ", " ", " "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
