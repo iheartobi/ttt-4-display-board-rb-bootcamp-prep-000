e18c3df5fb4b0a2a240688d5fd3171e31ea65008
Ititi Obidah <iobidah9@gmail.com>
2019-04-25 20:58:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,10 +5,10 @@ def display_board(board)
   pipe = "|"
   line = "-----------"
   
-  puts "#{board[0]} #{pipe} #{board[1]} #{pipe} #{board[1]}"
+  puts "#{board[0]} #{pipe} #{board[1]} #{pipe} #{board[2]}"
   puts line
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  puts "#{board[3]} #{pipe} #{board[4]} #{pipe} #{board[5]}"
+  puts line
+  puts "#{board[6]} #{pipe} #{board[7]} #{pipe} #{board[8]}"
 end 
 
